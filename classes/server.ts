81e421a09e0a100e8d087c9c31577886e82dfd26
Rosales993 <rosaleschavezj58@gmail.com>
2026-01-13 16:34:27 -0600

    Cambios App

--- classes/server.ts
+++ classes/server.ts
@@ -1,77 +1,49 @@
-
 import express from 'express';
 import { SERVER_PORT } from '../global/environment.js';
-import { Server as socketIO  }  from 'socket.io';
+import { Server as socketIO } from 'socket.io';
 import http from 'http';
 import * as socket from '../sockets/socket.js';
 
-export default class Server{
+export default class Server {
     public app: express.Application;
     public port: number;
-
     public io: socketIO;
     private httpServer: http.Server;
+    private static _instance: Server;
 
-    
-
-    private static _instance: Server; 
-
-
-
-    private constructor(){
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
 
-        this.httpServer = new http.Server( this.app);
-
-           // CONFIGURACIÓN DE CORS AQUÍ:
+        // CONFIGURACIÓN DE CORS ÚNICA - Permite conexión desde Angular
         this.io = new socketIO(this.httpServer, {
-        cors: {
-            origin: "http://localhost:4200", // Permite el origen de Angular
-            methods: ["GET", "POST"],         // Corregido: sin corchetes extra
-            credentials: true
-
-       }
-        
-    });   
-    
-    this.escucharSockets(); 
-
-    }
-
-    public static get instance(){
-        return this._instance || ( this._instance = new this());
-
-    }
-
-    escucharSockets(){
-        console.log('Escuchando conexiones - sockets');
-
-        this.io.on( 'connection', cliente  => {
-
-            console.log('Cliente conectado');  
-
-            //hacer llamado directo para mostrar en servidor------------------>
-            
-            // cliente.on('disconnect', () => {
-            //     console.log('Cliente Desconectado');
-            // });
-
-
-            //Mensajes
-            socket.mensaje( cliente, this.io);
-
-            //Desconectar
-            socket.desconectar( cliente);
-            
+            cors: {
+                origin: "http://localhost:4200",
+                methods: ["GET", "POST"],
+                credentials: true
+            }
         });
-        
+
+        this.escucharSockets();
+    }
+
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets() {
+        console.log('Escuchando conexiones - sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+
+            // Manejadores de eventos de sockets
+            socket.mensaje(cliente, this.io);
+            socket.desconectar(cliente);
+        });
     }
 
     start(callback: () => void) {
-        
-        this.httpServer.listen( this.port, callback);
-
+        this.httpServer.listen(this.port, callback);
     }
-
 }
